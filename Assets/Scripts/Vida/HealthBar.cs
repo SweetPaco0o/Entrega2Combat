1bb36836f0e8e71239d957e5caeadc61b21a6ab9
Monroigggg <joel.monroig@estudiantat.upc.edu>
2024-04-17 11:38:59 +0200

    Vida Actualitzada

--- Assets/Scripts/Vida/HealthBar.cs
+++ Assets/Scripts/Vida/HealthBar.cs
@@ -14,15 +14,10 @@ public class HealthBar : MonoBehaviour
     {
         slider.maxValue = health;
         slider.value = health;
-
-        fill.color = gradient.Evaluate(1f);
     }
 
     public void SetHealth(int health)
     {
         slider.value = health;
-
-        fill.color = gradient.Evaluate(slider.normalizedValue);
     }
-
 }
