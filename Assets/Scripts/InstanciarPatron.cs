7fc97ceacabd351c04ae00d53fb380dee2c9b8e3
SergiSabrosuras3000 <sergi.martinez.somolinos@estudiantat.upc.edu>
2024-05-05 21:38:38 +0200

    truño

--- Assets/Scripts/InstanciarPatron.cs
+++ Assets/Scripts/InstanciarPatron.cs
@@ -2,24 +2,21 @@ using UnityEngine;
 
 public class InstanciarPatron : MonoBehaviour
 {
-    public GameObject cubePrefab; 
+    public GameObject cubePrefab;
+    public LayerMask patronLayer; 
 
-    void Update()
+    void OnTriggerEnter(Collider other)
     {
-       
-        if (Input.GetKeyDown(KeyCode.E))
+        
+        if (((1 << other.gameObject.layer) & patronLayer) != 0)
         {
-            
             ShowCube();
         }
     }
 
     void ShowCube()
     {
-        
         GameObject cubeInstance = Instantiate(cubePrefab, transform.position, Quaternion.identity);
-
-        
         Destroy(cubeInstance, 5f);
     }
 }
